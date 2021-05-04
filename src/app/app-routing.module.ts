5655d96500397a8683b2bb1a72ac3d80ca7b2a55
Arturo Durazo <arturo.a.durazo@gmail.com>
2021-05-04 15:56:34 -0700

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component'
+import {AboutUsComponent} from './components/about-us/about-us.component'
+import {CatalogueComponent} from './components/catalogue/catalogue.component'
+import {CakesComponent} from './components/catalogue/cakes/cakes.component'
+import {DrinksComponent} from './components/catalogue/drinks/drinks.component'
+import {SpecialtiesComponent} from './components/catalogue/specialties/specialties.component'
+import {ContactUsComponent} from './components/contact-us/contact-us.component'
+import {LocationsComponent} from './components/locations/locations.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  //path is from where you wanna reach such component
+  //reach from root cuz its homepage
+  {path: '', component: HomePageComponent},
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'catalogue', component: CatalogueComponent},
+  {path: 'catalogue/cakes', component: CakesComponent},
+  {path: 'catalogue/drinks', component: DrinksComponent},
+  {path: 'catalogue/specialties', component: SpecialtiesComponent},
+  {path: 'contact-us', component: ContactUsComponent},
+  {path: 'locations', component: LocationsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { CatalogueComponent } from './components/catalogue/catalogue.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { LocationsComponent } from './components/locations/locations.component';
+import { CakesComponent } from './components/catalogue/cakes/cakes.component';
+import { DrinksComponent } from './components/catalogue/drinks/drinks.component';
+import { SpecialtiesComponent } from './components/catalogue/specialties/specialties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomePageComponent
+    HomePageComponent,
+    AboutUsComponent,
+    CatalogueComponent,
+    ContactUsComponent,
+    LocationsComponent,
+    CakesComponent,
+    DrinksComponent,
+    SpecialtiesComponent
   ],
   imports: [
     BrowserModule,

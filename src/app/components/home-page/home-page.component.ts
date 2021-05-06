47a726a867df0b60111abd6a7335e3abc9fcc91e
Leo <leo.tf.123@live.com>
2021-05-06 15:28:53 -0700

    added caroussel

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as M from 'materialize-css'
+
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
+
   ngOnInit(): void {
+  }
+
+
+  ngAfterViewInit(){
+    
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.carousel');
+      var instances = M.Carousel.init(elems, {
+        dist:0
+      });
+    });
   }
 
 }
